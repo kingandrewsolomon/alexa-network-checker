8bccf5405e5eb1fa9401adae518e7c6919bc1bf1
Andrew Solomon <andrewsolomon610@gmail.com>
2018-01-14 11:51:40 -0800

    changing port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let AlexaAppServer = require('alexa-app-server');
 
 let instance = AlexaAppServer.start({
     server_root: __dirname,
-    port: 8080,
+    port: processors.env.,
     preRequest: function(json, req, res) {
         console.log("preRequest fired");
         json.userDetails = { "name": "Andrew Solomon" };
